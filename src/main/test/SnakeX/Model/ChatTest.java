63c38358cf39ce2641e7db0d45d583423504949b
FlorisFeddema <9294266+FlorisFeddema@users.noreply.github.com>
2018-06-12 15:05:34 +0200

    added first test

---
+++ src/main/test/SnakeX/Model/ChatTest.java
@@ -0,0 +1,42 @@
+package SnakeX.Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChatTest {
+
+    Chat chat;
+    Player player;
+
+    @Before
+    public void setUp() throws Exception {
+        chat = new Chat();
+        player = new Player(null);
+    }
+
+    @Test
+    public void getLastMessage() {
+        Message message1 = new Message("yay", player);
+        Message message2 = new Message("ah", player);
+        chat.addMessage(message1);
+        chat.addMessage(message2);
+
+        Message result = chat.getLastMessage();
+
+        assertSame(message2, result);
+        assertNotSame(message1, result);
+    }
+
+    @Test
+    public void addMessage() {
+        Message message1 = new Message("yay", player);
+        chat.addMessage(message1);
+
+        Message result = chat.getLastMessage();
+
+        assertSame(message1, result);
+        assertEquals(message1.getText(), message1.getText());
+    }
+}
